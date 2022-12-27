f82efe9900322e744001cfda09cacaf12275a4e5
XeNoXTS <palmyunvisate2003@gmail.com>
2022-12-27 15:58:36 +0700

    Lab10_1 Completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 int main(){	
+	double loan, intpery, paypery;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> intpery;
 	cout << "Enter amount you can pay per year: ";
+	cin >> paypery;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +18,35 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
+
+	int EndOfYear = 1;
+	double PrevBalance = loan, Interest = (intpery/100)*loan, Total = PrevBalance + Interest, Payment = paypery, NewBalance;	
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	while(PrevBalance > 0)
+	{
+		cout << setw(13) << left << EndOfYear; 
+		cout << setw(13) << left << PrevBalance;
+		cout << setw(13) << left << Interest;
+		cout << setw(13) << left << Total;
+		if(Total >= paypery)
+		{
+			Payment = paypery;
+		}
+		else
+		{
+			Payment = Total;
+		}
+		cout << setw(13) << left << Payment;
+		NewBalance = Total - Payment;
+		if(NewBalance < 0) NewBalance = 0;
+		cout << setw(13) << left << NewBalance;
+		cout << "\n";
+		EndOfYear++;
+		PrevBalance	= NewBalance;
+		Interest = PrevBalance*(intpery/100);
+		Total = PrevBalance + Interest;
+		
+	};
 	
 	return 0;
 }
